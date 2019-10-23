99a1eee8fd83d2f2df2b735ec9467a459e968d23
Èrik García Rosa <erik@MacBook-Pro-de-Erik.local>
2019-10-23 14:34:08 +0200

    succes update page and redirection button

---
+++ resources/views/Events/successUpdate.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Event updated succesfully</title>
+</head>
+<body>
+    <h1>The event details have been updated succesfully!</h1>
+    <input type="button" value="Go back to event list" onclick="location.href='{{ url('event') }}'">
+</body>
+</html>
